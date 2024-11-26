6df799ce17b559442cd2c0ce1f1f03e7ea1c8c1d
raselworshop <raselworshop@gmail.com>
2024-11-26 21:06:46 +0800

    added the commit

--- coffee-store-client/src/component/AddCoffee.jsx
+++ coffee-store-client/src/component/AddCoffee.jsx
@@ -1,9 +1,97 @@
 import React from 'react';
 
 const AddCoffee = () => {
+    const handleAddCoffee = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = {
+            name,
+            chef,
+            supplier,
+            taste,
+            category,
+            details,
+            photo,
+        }
+        console.log(newCoffee)
+    }
     return (
-        <div>
-            <h3>Add Coffee</h3>
+        <div className='lg:w-3/4 mx-auto'>
+            <div className="text-center p-10">
+                <h1 className="text-5xl font-bold">Add Coffee!</h1>
+                <p className="py-6">
+                    Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                </p>
+            </div>
+            <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+                <form onSubmit={handleAddCoffee} className="card-body">
+                    {/* form first row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="coffee name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Chef</span>
+                            </label>
+                            <input type="text" name='chef' placeholder="chef name" className="input input-bordered" required />
+                        </div>
+                    </div>
+                    {/* form second row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Supplier</span>
+                            </label>
+                            <input type="text" name='supplier' placeholder="coffee supplier" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Taste</span>
+                            </label>
+                            <input type="text" name='taste' placeholder="taste name" className="input input-bordered" required />
+                        </div>
+                    </div>
+                    {/* form third row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Category</span>
+                            </label>
+                            <input type="text" name='category' placeholder="coffee Category" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Details</span>
+                            </label>
+                            <input type="text" name='details' placeholder="Coffee Details" className="input input-bordered" required />
+                        </div>
+                    </div>
+
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
+
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Add Coffee</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
